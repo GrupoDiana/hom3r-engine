0e2b67dd1f0e00675776bf6da79a39f8b192b5ac
Daniel Gonzalez Toledo <dgonzalezt@uma.es>
2019-05-17 11:19:58 +0200

    Solves label reset when product model is reseted

--- LabelEventReceiver.cs
+++ LabelEventReceiver.cs
@@ -90,7 +90,9 @@ public class LabelEventReceiver : MonoBehaviour {
                     }
                     break;
                 case TCoreEvent.ModelManagement_ResetModel:
-                    hom3r.coreLink.Do(new CLabelCommand(TLabelCommands.RemoveAllLabelOfConfirmedObjects), Constants.undoNotAllowed);
+                    //hom3r.coreLink.Do(new CLabelCommand(TLabelCommands.RemoveAllLabelOfConfirmedObjects), Constants.undoNotAllowed);
+                    //hom3r.quickLinks.scriptsObject.GetComponent<LabelCommandReceiver>().ExecuteRemoveAllLabelOfConfirmedGOList();
+                    hom3r.quickLinks.scriptsObject.GetComponent<LabelManager>().RemoveAllLabels();                    
                     break;
 
 
